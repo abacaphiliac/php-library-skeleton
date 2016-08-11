f093fbc29eaf87b646066a6d9dcaeb5fd45ffa1d
Tim Younger <tim@webpt.com>
2016-08-10 21:32:49 -0700

    strip slashes from encoded JSON.

--- src/PhpLibrarySkeleton/File/JsonFile.php
+++ src/PhpLibrarySkeleton/File/JsonFile.php
@@ -44,6 +44,6 @@ class JsonFile extends AbstractFile
      */
     protected function encode(array $data)
     {
-        return stripslashes($this->serializer->encode($data, JsonEncoder::FORMAT));
+        return str_replace('\/', '/', $this->serializer->encode($data, JsonEncoder::FORMAT));
     }
 }
